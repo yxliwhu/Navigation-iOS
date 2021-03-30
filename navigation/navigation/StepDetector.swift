1a8579c728ed5ea1e51dae5d9cd47d7b1033a4fc
Li Yaxin <16902456r@connect.polyu.hk>
2021-03-30 11:53:51 +0800

    Make Step counter working

--- navigation/navigation/StepDetector.swift
+++ navigation/navigation/StepDetector.swift
@@ -13,7 +13,6 @@ class StepDetector {
     var data:Data
     var userHeight:String
     var kalmanPositionDetector:KalmanPositionDetector
-    static let NS2S:Float = 1.0 / 1000000000.0
     
     init(_ userHeight:String,_ realtime:Bool){
         self.data = Data()
@@ -30,11 +29,9 @@ class StepDetector {
         return ((radians) * (180.0 / Double.pi))
     }
     
-    //        float AccX,AccY,AccZ,MagX,MagY,MagZ,GyroX,GyroY,GyroZ,LightX
     func onSensorChanged(_ sensorEvent:DeviceData) {
         if (Realtime) {
             self.kalmanPositionDetector.initSensorChange()
-            ////todo: Get sensor data
             let acc = sensorEvent.accelerometer
             data.setAccX(Float(acc!.acceleration.x))
             data.setAccY(Float(acc!.acceleration.y))
@@ -44,16 +41,18 @@ class StepDetector {
             let Rotation:[Float] = [Float(rmat.m11),Float(rmat.m12),Float(rmat.m13),
                                     Float(rmat.m21),Float(rmat.m22),Float(rmat.m23),
                                     Float(rmat.m31),Float(rmat.m32),Float(rmat.m33)]
-            
-            
             self.data.setRotationMatrix(Rotation)
-            let za = self.kalmanPositionDetector.getAlgorithm().zValueOfAcceleration(data, Rotation)// Get the vertical accelerate
+            
+            //codes for step counter start****************************************
+            let userAcc = sensorEvent.motion!.userAcceleration
+            let za = Float(userAcc.z * 9.8)// Get the vertical accelerate
             var lp = self.kalmanPositionDetector.getLp()
             lp = za * 0.2 + lp *  0.8
             self.kalmanPositionDetector.setLp(lp)
             let step = self.kalmanPositionDetector.stepCounting(lp)
             self.kalmanPositionDetector.setStep(step)
             self.data.setStepNumOwn(Float(step))
+            //codes for step counter end *****************************************
             
             let meg = sensorEvent.magnetic!.magneticField
             let megVals:[Float] = [Float(meg.x),Float(meg.y),Float(meg.z)]
@@ -62,20 +61,17 @@ class StepDetector {
             let accVals:[Float] = [Float(acc!.acceleration.x),Float(acc!.acceleration.y),Float(acc!.acceleration.z)]
             data.setAccelemeterValues(accVals)
             
+            //Get the heading info from magnetometer
+            let magnHeading = self.kalmanPositionDetector.getMagnHeading
+            data.setDegree(Float(magnHeading()))
             
-            
-            let magnHeading = self.kalmanPositionDetector.getmValues()[0] + 0.0521853
-            self.kalmanPositionDetector.setMagnHeading(Double(magnHeading))
-            let dv:Float = Float(self.toDegrees(Double(magnHeading)) + 360.0)
-            let dvv = dv.truncatingRemainder(dividingBy: 360.0)
-            self.data.setDegree(dvv)
-            
-            let gyro = sensorEvent.gyro!.rotationRate
+            let gyro = sensorEvent.motion!.rotationRate
+    
             let gyroVals:[Float] = [Float(gyro.x),Float(gyro.y),Float(gyro.z)]
             self.data.setGyroscopeValues(gyroVals)
             // This time step's delta rotation to be multiplied by the current rotation
             // after computing it from the gyro sample data.
-            let dT = Float(MotionHelper.sampleTime) * StepDetector.NS2S
+            let dT = Float(MotionHelper.sampleTime) / 1000.0
             let axisRawGryo = data.getGyroscopeValues()
             var axisConvert:[Float] = [Float](repeating: 0, count: 3)//convert gyro using system matrix
             axisConvert[0] =  Float(toDegrees(Double(self.kalmanPositionDetector.getAlgorithm().toEast(axisRawGryo, data.getRotationMatrix())))) * dT//Convert to horizontal altitude
@@ -85,7 +81,7 @@ class StepDetector {
             let axisZAngle = axisConvert[2]
             var MygyroHeading = self.kalmanPositionDetector.getMygyroHeading()
             MygyroHeading = MygyroHeading + axisZAngle //Gyro integration angle
-            self.kalmanPositionDetector.setMagnHeading(Double(MygyroHeading))
+//            self.kalmanPositionDetector.setMagnHeading(Double(MygyroHeading))
             data.setTurnAnlge(self.kalmanPositionDetector.CalculateGyroMax(self.kalmanPositionDetector.getCurrentTime(), axisZAngle)) //Calculate trun angle and set it to data
             
             var x_angle = KalmanCalculate.kalmanCalculate(data.getDegree(),axisZAngle,dT)// Kalman fiter heading got from accelerometer, magnerometer and gyroscope
@@ -98,7 +94,7 @@ class StepDetector {
         }
         
         
-        self.kalmanPositionDetector.calculate()
+        let _ = self.getDetector().calculate()
     }
 }
 
